b92ca79976e4196bbe228f77bb7e19be3b4b733d
Darya <lyubinadarya@gmail.com>
2023-12-08 09:53:57 +0100

    create project

---
+++ NFTMarcetplace/NFTMarcetplace/NFTMarcetplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTMarcetplaceApp.swift
+//  NFTMarcetplace
+//
+//  Created by Darya Lyubina  on 08.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NFTMarcetplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
