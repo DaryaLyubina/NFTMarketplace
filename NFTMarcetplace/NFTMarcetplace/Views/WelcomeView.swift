8e3f801715356723688200671995d46c177f093c
Darya <lyubinadarya@gmail.com>
2024-02-07 13:42:53 +0100

    add navigation to WelcomeView

--- NFTMarcetplace/NFTMarcetplace/Views/WelcomeView.swift
+++ NFTMarcetplace/NFTMarcetplace/Views/WelcomeView.swift
@@ -9,34 +9,36 @@ import SwiftUI
 
 struct WelcomeView: View {
   var body: some View {
-    ZStack {
-      LinearGradient
-        .welcomeBackground
-        .ignoresSafeArea()
+    NavigationStack {
+      ZStack {
+        LinearGradient
+          .welcomeBackground
+          .ignoresSafeArea()
 
-      Image("WelcomeBackground", bundle: nil)
-        .resizable()
-        .scaledToFill()
-        .ignoresSafeArea()
-        .offset(x: 0, y: 70)
+        Image("WelcomeBackground", bundle: nil)
+          .resizable()
+          .scaledToFill()
+          .ignoresSafeArea()
+          .offset(x: 0, y: 70)
 
-      VStack(alignment: .leading) {
-        Spacer()
-          .frame(height: 20)
+        VStack(alignment: .leading) {
+          Spacer()
+            .frame(height: 20)
 
-        Text("Welcome to \nNFT Marketplace")
-          .font(.largeTitle)
-          .fontWeight(.bold)
-          .foregroundColor(.white)
-          .multilineTextAlignment(.leading)
-          .shadow(radius: 1, x: 0, y: 4)
-          .shadow(color: .black, radius: 1)
+          Text("Welcome to \nNFT Marketplace")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .multilineTextAlignment(.leading)
+            .shadow(radius: 1, x: 0, y: 4)
+            .shadow(color: .black, radius: 1)
 
-        Spacer()
+          Spacer()
 
-        WelcomeMessageFooter()
+          WelcomeMessageFooter()
+        }
+        .padding()
       }
-      .padding()
     }
   }
 }
@@ -49,23 +51,23 @@ struct WelcomeView_Previews: PreviewProvider {
 
 struct WelcomeMessageFooter: View {
   var body: some View {
-    VStack(alignment: .center) {
-      Text("Explore and Mint NFTs")
-        .font(.title2)
-        .fontWeight(.bold)
-        .foregroundColor(.white)
-        .padding()
-        .kerning(0.35)
+    VStack(alignment: .center, spacing: 25) {
+      VStack(spacing: 7.0) {
+        Text("Explore and Mint NFTs")
+          .font(.title2)
+          .fontWeight(.bold)
+          .foregroundColor(.white)
+          .kerning(0.35)
         .multilineTextAlignment(.center)
 
       Text(" You can buy and sell the NFTs of the best artists in the world.")
         .font(.caption2)
         .foregroundColor(.white)
-        .padding()
         .multilineTextAlignment(.center)
+      }
 
-      Button("Get started now") {
-
+      NavigationLink("Get Started") {
+        HomeView()
       }
       .buttonStyle(WelcomeButtonStyle())
     }
@@ -76,7 +78,6 @@ struct WelcomeMessageFooter: View {
       RoundedRectangle(cornerRadius: 30)
         .inset(by: 0.5)
         .stroke(.white.opacity(0.3), lineWidth: 1)
-
     )
   }
 }
